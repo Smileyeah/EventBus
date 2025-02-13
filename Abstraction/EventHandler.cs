3db3056520f5b75ba083a7b2db15b019808e4cb2
lihao <lihao@ganweisoft.com>
2025-02-13 10:55:10 +0800

    feat: handler exception

--- Abstraction/EventHandler.cs
+++ Abstraction/EventHandler.cs
@@ -11,6 +11,21 @@ namespace GWCloudPortal.EventBus.Abstraction
         public abstract Task<bool> HandleAsync(T @event, CancellationToken cancellationToken = default);
 
         public Task<bool> HandleAsync(IEvent @event, CancellationToken cancellationToken = default)
-            => CanHandle(@event) ? HandleAsync((T)@event, cancellationToken) : Task.FromResult(false);
+        {
+            var handleState = false;
+            if (CanHandle(@event))
+            {
+                try
+                {
+                    handleState = HandleAsync((T)@event, cancellationToken).Result;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            
+            return Task.FromResult(handleState);
+        }
     }
 }
