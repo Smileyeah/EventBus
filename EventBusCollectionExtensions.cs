85ea873388b73abc89edad5a497e54150ea3e693
lihao <lihao@ganweisoft.com>
2025-02-13 10:36:36 +0800

    feat: commit service ext

---
+++ EventBusCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using GWCloudPortal.EventBus.Default;
+using GWCloudPortal.EventBus.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GWCloudPortal.EventBus;
+
+public static class EventBusCollectionExtensions
+{
+    public static IServiceCollection AddEventBus(this IServiceCollection services)
+    {
+        var eventHandlerExecutionContext = new EventHandlerExecutionContext(services, 
+            sc => sc.BuildServiceProvider());
+        
+        services.AddSingleton<IEventHandlerExecutionContext>(eventHandlerExecutionContext);
+        services.AddSingleton<IEventBus, PassThroughEventBus>();
+
+        return services;
+    }
+}
